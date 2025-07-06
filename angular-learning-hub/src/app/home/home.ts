a8d42543b351d663460be2780701605ff9bc423e
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-06 11:21:18 +0300

    add greeting component with eventListeners and signals

--- angular-learning-hub/src/app/home/home.ts
+++ angular-learning-hub/src/app/home/home.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Greeting],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
+  homeMessage = signal('hello, world form home!');
 
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
 }
